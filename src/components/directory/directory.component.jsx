c22e0011a8ecb24f4fde176c46b09d9a7aa64ad3
martinstereo <martinsteiro02@gmail.com>
2024-11-15 15:57:19 +0100

    added routing to home page

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,13 +1,15 @@
-import './categories-menu.styles.scss';
+import './directory.styles.scss';
+
 import CategoryItem from '../category-item/category-item.component';
-const CategoriesMenu = ({ categories }) => {
-  return(
+
+const Directory = ({ categories }) => {
+  return (
     <div className='categories-container'>
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default CategoriesMenu
+export default Directory
