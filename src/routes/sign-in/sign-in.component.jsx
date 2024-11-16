2409f7bdc67681f777d6bfe573123f4930ea6f2c
martinstereo <martinsteiro02@gmail.com>
2024-11-17 00:16:28 +0100

    Google Popup login auth button with firebase

---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils'
+
+const SignIn = () => {
+
+  // Get google sign-in auth from firebase
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  }
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>
+        Sign in with Google
+      </button>
+    </div>
+  )
+};
+
+export default SignIn;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,30 @@
+import { initializeApp } from "firebase/app";
+
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAru-6Vl02WEsCTywA9zXkfxgnsKXhfxS8",
+  authDomain: "mirror-clothing-db.firebaseapp.com",
+  projectId: "mirror-clothing-db",
+  storageBucket: "mirror-clothing-db.firebasestorage.app",
+  messagingSenderId: "592357640508",
+  appId: "1:592357640508:web:a206922e421412782d3639"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
