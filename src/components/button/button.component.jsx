ffc539c48c1ccc983d90a176486837200b1b2471
martinstereo <martinsteiro02@gmail.com>
2024-11-24 20:24:34 +0100

    Add cart and product context; implement cart dropdown and icon components; enhance shop layout with product cards

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -1,20 +1,18 @@
-import React from 'react';
-
-import "./button.styles.scss";
+import './button.styles.scss';
 
 const BUTTON_TYPE_CLASSES = {
   google: 'google-sign-in',
-  inverted: 'inverted'
-}
+  inverted: 'inverted',
+};
 
 const Button = ({ children, buttonType, ...otherProps }) => {
   return (
-    <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
-      {...otherProps}
-    >
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}>
       {children}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,32 +4,38 @@ import { ReactComponent as MirrorLogo } from '../../assets/mirror.svg';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 
 import './navigation.styles.scss';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 const NavigationBar = () => {
   const { currentUser } = useContext(UserContext);
+  const { isCartOpen } = useContext(CartContext);
 
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
-          <MirrorLogo className="logo" />
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <MirrorLogo className='logo' />
         </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
             <h3>SHOP</h3>
           </Link>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>
+            <span className='nav-link' onClick={signOutUser}>
               <h3>SIGN OUT</h3>
             </span>
           ) : (
-            <Link className="nav-link" to="/auth">
+            <Link className='nav-link' to='/auth'>
               <h3>SIGN IN</h3>
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
