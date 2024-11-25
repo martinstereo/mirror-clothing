84fd47615d36a62a5ca603d2c110cdcb3eebae9e
martinstereo <martinsteiro02@gmail.com>
2024-11-25 22:07:12 +0100

    Enhance cart functionality: add cartCount to CartContext, update CartIcon to display item count, and improve CartItem layout with image and price details

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
 
 export const AddCartItem = (cartItems, productToAdd) => {
   const foundCartItem = cartItems.find(
@@ -20,17 +20,33 @@ export const CartContext = createContext({
   setIsCartOpen: () => {},
   cartItems: [],
   addItemsToCart: () => {},
+  cartCount: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+    setCartCount(newCartCount);
+  }, [cartItems]);
 
   const addItemToCart = (productToAdd) => {
     setCartItems(AddCartItem(cartItems, productToAdd));
   };
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    cartItems,
+    cartCount,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
