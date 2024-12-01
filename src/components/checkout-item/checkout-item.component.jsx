d3213de9b013a471da4055bc22da7c201369adf9
martinstereo <martinsteiro02@gmail.com>
2024-12-01 15:49:04 +0100

    Format price display in CheckoutItem component to include a dollar sign

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -27,7 +27,7 @@ const CheckoutItem = ({ cartItem }) => {
           &#10095;
         </div>
       </span>
-      <span className='price'>{price}</span>
+      <span className='price'>$ {price}</span>
       <div className='remove-button' onClick={handleClearCartItem}>
         &#10005;
       </div>
