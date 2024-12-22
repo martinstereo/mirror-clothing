5e8bddaff95061b9509942e49c40d9e93f0097bb
martinstereo <martinsteiro02@gmail.com>
2024-12-22 18:15:25 +0100

    hey

--- src/routes/authentication/authentication.styles.jsx
+++ src/routes/authentication/authentication.styles.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const AuthenticationContainer = styled.div`
   display: flex;
-  width: 900px;
+  width: 1000px;
   justify-content: space-between;
-  margin: 30px;
+  margin: 40px;
 `;
